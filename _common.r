cf96ca5c8dbf0938f5537a450791d5d0b244ac10
aleruete <aleruete@gmail.com>
2021-05-11 15:47:48 +0200

    commit commit

--- _common.r
+++ _common.r
@@ -1,6 +1,6 @@
+
 knitr::opts_knit$set(root.dir = here::here())
 set.seed(1)
-options(digits = 3)
 
 knitr::opts_chunk$set(
   comment = "#>",
@@ -15,5 +15,15 @@ knitr::opts_chunk$set(
   fig.asp = 0.618,  # 1 / phi
   fig.show = "hold"
 )
-options(knitr.kable.NA = "")
-options(dplyr.print_min = 6, dplyr.print_max = 6)
+options(knitr.kable.NA = "",
+        dplyr.print_min = 6, dplyr.print_max = 6,
+        htmltools.dir.version = FALSE, formatR.indent = 2,
+        # width = 72, 
+        digits = 4, 
+        # widgetframe_widgets_dir = 'widgets',
+        warnPartialMatchAttr = FALSE,
+        warnPartialMatchDollar = FALSE)
+
+lapply(c('xfun'), function(pkg) {
+  if (system.file(package = pkg) == '') install.packages(pkg)
+})
